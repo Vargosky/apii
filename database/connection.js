3d9c835b27f8d83f791f608cd1ca07e4fa75add2
Vargosky <sarem.vargas@gmail.com>
2023-09-22 21:45:53 -0300

    primer

---
+++ database/connection.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const connection = async ()=>{
+
+    const bdName = "Damasco";
+    const urlLocal = "mongodb://127.0.0.1:27017/";
+    const urlCloud = "mongodb+srv://saremvargas:Sarem1509@cluster0.j4tuv0s.mongodb.net/";
+
+    try {
+        await mongoose.connect(urlCloud+bdName);
+        console.log("conectado correctamente a la BASE DE DATOS :"+bdName +"en la direccion "+urlCloud);
+    } catch (error) {
+        console.log(error);
+        throw new error ("no se pudo conectar a base de datos: "+bdName);
+    }
+
+
+}
+
+module.exports = connection;
---
+++ index.js
@@ -0,0 +1,40 @@
+const connection = require("./database/connection.js");
+const express = require("express");
+const cors = require("cors");
+//crear el servidor
+
+const app = express();
+const port = 3001;
+
+
+// Configurar CORS para permitir cualquier origen
+app.use(cors({
+    origin: (origin, callback) => {
+        // Permitir cualquier origen
+        callback(null, true);
+    },
+    credentials: true,
+}));
+
+
+app.get("/", (req,res)=>{
+    res.send('funcionando el servidor basico')
+})
+
+
+// Convertir todo a JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Conexión a la base de datos
+console.log("BIENVENIDO A LA API CTM");
+connection();
+
+
+
+
+
+// Hacer que el servidor escuche
+app.listen(port, () => {
+    console.log(`Servidor de Node corriendo en el puerto ${port}`);
+});
