3a837b751e615eeec494c79d22e22fa39b8794cb
Vargosky <sarem.vargas@gmail.com>
2023-09-23 16:12:10 -0300

    rollback

--- index.js
+++ index.js
@@ -1,48 +1,62 @@
-const connectToDatabase = require("./database/connection.js");
+const connection = require("./database/connection.js");
 const express = require("express");
 const cors = require("cors");
+//crear el servidor
 
 const app = express();
 const port = 3001;
 
-// Función para inicializar el servidor y la conexión a la base de datos
-const initializeServer = async () => {
-    try {
-        // Intentar establecer conexión con la base de datos
-        await connectToDatabase();
 
-        console.log("BIENVENIDO A LA API CTM");
+// Conexión a la base de datos
+console.log("BIENVENIDO A LA API CTM");
+connection();
 
-        // Ruta principal para verificar si la API está corriendo
-        app.get("/", (req, res) => {
-            res.send('API conectada y funcionando');
-        });
+if (connection) {
 
-        // Rutas
-        const routeMateriaPrima = require("./api/routes/routeMateriaPrima.js");
-        app.use("/mmpp", routeMateriaPrima);
+    app.get("/", (req,res)=>{
+        res.send(' conectado');
+    })
 
-        // Configurar CORS para permitir cualquier origen
-        app.use(cors({
-            origin: (origin, callback) => {
-                // Permitir cualquier origen
-                callback(null, true);
-            },
-            credentials: true,
-        }));
+}
+else{
 
-        // Convertir todo a JSON
-        app.use(express.json());
-        app.use(express.urlencoded({ extended: true }));
-
-        // Iniciar el servidor en el puerto especificado
-        app.listen(port, () => {
-            console.log(`Servidor de Node corriendo en el puerto ${port}`);
-        });
-    } catch (error) {
-        console.error("No se ha podido conectar:", error.message);
-    }
 }
 
-// Iniciar la aplicación
-initializeServer();
+//rutas
+
+const routeMateriaPrima= require("./api/routes/routeMateriaPrima.js");
+
+app.use("/mmpp", routeMateriaPrima);
+
+
+
+
+
+
+// Configurar CORS para permitir cualquier origen
+app.use(cors({
+    origin: (origin, callback) => {
+        // Permitir cualquier origen
+        callback(null, true);
+    },
+    credentials: true,
+}));
+
+
+
+
+
+
+// Convertir todo a JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+
+// Hacer que el servidor escuche
+app.listen(port, () => {
+    console.log(`Servidor de Node corriendo en el puerto ${port}`);
+});
