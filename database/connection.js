edad744b6a2c8a895cb9c2db62729df7745711c2
Vargosky <sarem.vargas@gmail.com>
2023-09-23 16:13:31 -0300

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,21 +1,31 @@
 const mongoose = require("mongoose");
 
-const connection = async ()=>{
+// Función asíncrona para establecer conexión con MongoDB
+const connectToDatabase = async () => {
+    // Nombre de la base de datos
+    const dbName = "Damasco";
 
-    const bdName = "Damasco";
+    // URL de conexión local y en la nube
+    // Nota: Es peligroso dejar las credenciales directamente en el código
+    // Considera usar variables de entorno para manejar esto de manera segura
     const urlLocal = "mongodb://127.0.0.1:27017/";
     const urlCloud = "mongodb+srv://saremvargas:Sarem1509@cluster0.j4tuv0s.mongodb.net/";
-    
 
     try {
-        await mongoose.connect(urlCloud+bdName);
-        console.log("conectado correctamente a la BASE DE DATOS :"+bdName +"en la direccion "+urlCloud);
+        // Intentar conectar a MongoDB usando la URL en la nube
+        await mongoose.connect(urlCloud + dbName, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        
+        console.log(`Conectado correctamente a la base de datos: ${dbName} en la dirección: ${urlCloud}`);
+        
     } catch (error) {
-        console.log(error);
-        throw new error ("no se pudo conectar a base de datos: "+bdName);
+        // En caso de error, registrar el error y lanzar un nuevo Error
+        console.error(`Error al conectar a la base de datos: ${dbName}`);
+        throw new Error(error.message);
     }
-
-
 }
 
-module.exports = connection;
+// Exportar la función para poder usarla en otros archivos
+module.exports = connectToDatabase;
