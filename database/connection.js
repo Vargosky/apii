c0a2922e4691d045d1cb244d5ce0a84aefdcd1ff
Vargosky <sarem.vargas@gmail.com>
2023-09-23 15:44:28 -0300

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -4,7 +4,7 @@ const connection = async ()=>{
 
     const bdName = "Damasco";
     const urlLocal = "mongodb://127.0.0.1:27017/";
-    const urlCloud = "mongodb+srv://saremvargas:Sarem1509@cluster0.j4tuv0s.mongodb.net/?retryWrites=true&w=majority";
+    const urlCloud = "mongodb+srv://saremvargas:Sarem1509@cluster0.j4tuv0s.mongodb.net/";
     
 
     try {
