d386f0d748e06c602c7e3c9743bfcc99217a1f97
Vargosky <sarem.vargas@gmail.com>
2023-09-24 12:54:06 -0300

    Update ctrlMateriaPrima.js

--- api/controllers/ctrlMateriaPrima.js
+++ api/controllers/ctrlMateriaPrima.js
@@ -2,7 +2,9 @@ const RawMaterial = require('../model/MateriaPrima');
 
 const getAllRawMaterials = async (req, res) => {
     try {
-        const rawMaterials = await RawMaterial.find().sort({ name: 1 });
+        console.log("Iniciando consulta a MongoDB Atlas...");
+        const rawMaterials = await RawMaterial.find().sort({ name: 1 }).limit(10);
+        console.log("Consulta exitosa, devolviendo datos...");
         res.status(200).json(rawMaterials);
     } catch (error) {
         res.status(500).json({ message: error.message });
